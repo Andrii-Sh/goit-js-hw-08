72115ab4d4b10c0195275cceab8edc9ef06845ca
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-03-07 17:36:26 +0200

    task-02 started

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+import Player from "@vimeo/player";
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onTimeupdate = function (data) { 
+    const currentTime = data.seconds;
+    console.log(currentTime);
+};
+
+const throttledOnTimeupdate = throttle(onTimeupdate, 1000);
+
+player.on('timeupdate', throttledOnTimeupdate);
+
