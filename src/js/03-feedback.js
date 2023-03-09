9d6733a8bcf7b81b716e3898479e3f56d8425bb1
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-03-09 13:46:40 +0200

    task-03 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
 
+const throttle = require('lodash.throttle');
+
+const feedBackObj = {};
+
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('.feedback-form input');
+const messageEl = document.querySelector('.feedback-form textarea');
+
+const FEEDBACK_FORM_KEY = "feedback-form-state";
+
+formEl.addEventListener('input', throttle(handleFormInput, 500));
+formEl.addEventListener('submit', handleFormSubmit);
+
+updateForm();
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+    console.log("Email: ", emailEl.value);
+    console.log("Message: ", messageEl.value);
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(FEEDBACK_FORM_KEY);
+}
+
+function handleFormInput(evt) {
+    createFeedbackObj(evt);
+    saveFeedbackOdj(feedBackObj);
+}
+
+function createFeedbackObj(evt) {
+    feedBackObj[evt.target.name] = evt.target.value;
+};
+
+function saveFeedbackOdj(obj) {
+    localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(obj));
+}
+
+function updateForm() {
+    if (localStorage.getItem(FEEDBACK_FORM_KEY)) {
+        const savedFeedbackObj = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+        emailEl.value = savedFeedbackObj.email;
+        messageEl.value = savedFeedbackObj.message;
+    }
+}
+
